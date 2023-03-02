6cd5b69928bb23e46cedde4eb97073bb7503ccd5
Riddhiman <shubro17@outlook.com>
2023-03-02 21:49:35 +0530

    Added routes for editing

--- routes/routes.js
+++ routes/routes.js
@@ -10,5 +10,5 @@ router.post('/logout',controllers.logout)
 router.post('/allusers',verifymiddleware.verifyJwt,controllers.allusers)
 router.post('/alladmins',checkadmin.checkadmin,verifymiddleware.verifyJwt,controllers.alladmins)
 router.post('/edituser',verifymiddleware.verifyJwt,controllers.updateuser)
-router.post('/editadmin',checkadmin.checkadmin,controllers.updateadmin)
+router.post('/editadmin',checkadmin.checkadmin,verifymiddleware.verifymiddleware,controllers.updateadmin)
 module.exports=router 
